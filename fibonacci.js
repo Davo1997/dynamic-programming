e9154ae7e790810ab3891688719f92b9efde36a7
David <41725656+Davo1997@users.noreply.github.com>
2021-12-08 17:34:46 +0400

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,8 @@
 // Traditional method using recursion
 
 function fib(a) {
-  if(a === 0) {
-    return 0;
-  }
-  if(a === 1) {
-    return 1;
+  if(a <= 1) {
+    return a;
   }
   return fib(a-1) + fib(a-2);
 }
