84a53602b2254ba47bf8555614f2b42b4533819f
David <41725656+Davo1997@users.noreply.github.com>
2021-12-08 17:38:44 +0400

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,8 @@
 // Traditional method using recursion
 
+let counter = 0;
 function fib(a) {
+  counter++;
   if(a <= 1) {
     return a;
   }
@@ -8,9 +10,10 @@ function fib(a) {
 }
 
 console.log(fib(10)) // 55
+console.log(counter); // number of operations - 177
 
 // Using dynamic programming (saving pre-calculated entities)
-
+let dynCounter = 0;
 const preCalculated = {
   0: 0,
   1: 1,
@@ -21,9 +24,11 @@ function fibDynamic(a) {
     return preCalculated[a];
   }
   for(let i = 2; i < a + 1; i++) { // starting from 2nd index, because 0th and 1st are already filled in our object
+    dynCounter++;
     preCalculated[i] = preCalculated[i-1] + preCalculated[i-2];
   }
   return preCalculated[a];
 }
 
 console.log(fibDynamic(10)) // 55
+console.log(dynCounter) // number of operations - 9 ( complexity -> O(N) )
