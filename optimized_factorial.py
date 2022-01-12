c9d320285cb533fe2cf1ee97c8260022624ef1c5
David <41725656+davidgabrielyan@users.noreply.github.com>
2022-01-12 12:02:31 +0400

    Update optimized_factorial.py

--- optimized_factorial.py
+++ optimized_factorial.py
@@ -1,7 +1,6 @@
 """
 This is a function for fastly counting factorial of any
-number (up to 100.000 and more (works in 5-10 seconds)) without
-recursion, with list - values in it
+number without recursion, with list - values in it
 """
 
 def fac(n):
